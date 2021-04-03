1772f0b29e3e59c453bb409cc629d287e2827b8d
devsam-bit <devsam-bit@outlook.com>
2021-04-03 17:43:00 -0400

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand:{
+          DEFAULT: "#0D0D2B",
+          button: "#3671E9",
+          white10: "#ffffff1A",
+          icon: "#3671E9"
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
